a422c3421555bab768e9dc2658a55fdf5debadd1
Jae Hun Lee <jaehunlee@Jaeui-MacBookPro.local>
2024-06-16 15:37:25 +0900

    Add AppleSDGothicNeo font and update font path

--- image_generator.py
+++ image_generator.py
@@ -3,8 +3,6 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import summarizer  # summarizer 모듈을 가져옴
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "amplified-grail-426504-k8-fe1e47809d84.json"
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 app.secret_key = 'your_secret_key'
@@ -35,44 +33,49 @@ def summarize():
 
 @app.route('/generate', methods=['POST'])
 def generate_image():
-    summarized_text = request.form.getlist('sentence')
-    font_sizes = request.form.getlist('fontsize')
-    image_path = session.get('image_path')
-    generated_images = []
+    try:
+        summarized_text = request.form.getlist('sentence')
+        font_sizes = request.form.getlist('fontsize')
+        image_path = session.get('image_path')
+        generated_images = []
 
-    for i, (sentence, font_size) in enumerate(zip(summarized_text, font_sizes)):
-        image = Image.open(image_path)
-        draw = ImageDraw.Draw(image)
-        
-        # 폰트 크기를 사용자가 지정한 크기로 설정
-        font_path = "AppleSDGothicNeo.ttc"
-        font = ImageFont.truetype(font_path, size=int(font_size))
-        
-        # 사용자가 지정한 줄바꿈을 그대로 사용
-        lines = sentence.split('\n')
-        max_width = max(draw.textlength(line, font=font) for line in lines)
-        line_height = (draw.textbbox((0, 0), 'A', font=font)[3] - draw.textbbox((0, 0), 'A', font=font)[1]) * 2  # 줄 간격을 두 배로 조정
-        total_height = line_height * len(lines)
+        for i, (sentence, font_size) in enumerate(zip(summarized_text, font_sizes)):
+            image = Image.open(image_path)
+            draw = ImageDraw.Draw(image)
+            
+            # 프로젝트 폴더 내의 AppleSDGothicNeo 폰트 파일을 참조
+            font_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fonts', 'AppleSDGothicNeo.ttc')
+            font = ImageFont.truetype(font_path, size=int(font_size))
+            
+            # 사용자가 지정한 줄바꿈을 그대로 사용
+            lines = sentence.split('\n')
+            max_width = max(draw.textlength(line, font=font) for line in lines)
+            line_height = (draw.textbbox((0, 0), 'A', font=font)[3] - draw.textbbox((0, 0), 'A', font=font)[1]) * 2  # 줄 간격을 두 배로 조정
+            total_height = line_height * len(lines)
 
-        # 중앙 위치 계산 (이미지의 중앙에서 텍스트의 절반 너비와 높이를 뺀 위치)
-        text_x = (image.width - max_width) / 2
-        text_y = (image.height - total_height) / 2
+            # 중앙 위치 계산 (이미지의 중앙에서 텍스트의 절반 너비와 높이를 뺀 위치)
+            text_x = (image.width - max_width) / 2
+            text_y = (image.height - total_height) / 2
 
-        # 텍스트 배경 그리기 및 텍스트 그리기
-        current_y = text_y
-        for line in lines:
-            bbox = draw.textbbox((text_x, current_y), line, font=font)
-            background_position = (bbox[0] - int(int(font_size) * 0.28), bbox[1] - int(int(font_size) * 0.14), bbox[2] + int(int(font_size) * 0.28), bbox[3] + int(int(font_size) * 0.14))
-            draw.rectangle(background_position, fill="black")
-            draw.text((text_x, current_y), line, font=font, fill="white")
-            current_y += line_height
+            # 텍스트 배경 그리기 및 텍스트 그리기
+            current_y = text_y
+            for line in lines:
+                bbox = draw.textbbox((text_x, current_y), line, font=font)
+                background_position = (bbox[0] - int(int(font_size) * 0.28), bbox[1] - int(int(font_size) * 0.14), bbox[2] + int(int(font_size) * 0.28), bbox[3] + int(int(font_size) * 0.14))
+                draw.rectangle(background_position, fill="black")
+                draw.text((text_x, current_y), line, font=font, fill="white")
+                current_y += line_height
 
-        output_image_filename = f'output_{i+1}_{os.path.basename(image_path)}'
-        output_image_path = os.path.join(app.config['UPLOAD_FOLDER'], output_image_filename)
-        image.save(output_image_path, quality=95)
-        generated_images.append(output_image_filename)
+            output_image_filename = f'output_{i+1}_{os.path.basename(image_path)}'
+            output_image_path = os.path.join(app.config['UPLOAD_FOLDER'], output_image_filename)
+            image.save(output_image_path, quality=95)
+            generated_images.append(output_image_filename)
 
-    return render_template('result.html', image_urls=generated_images)
+        return render_template('result.html', image_urls=generated_images)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Internal Server Error", 500
 
 @app.route('/download/<filename>')
 def download_file(filename):
